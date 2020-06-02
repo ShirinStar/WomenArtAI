793a05fd19ec899ff1ec9b92d3a7395e4fb3465c
shirin <shirinanlen@gmail.com>
2020-06-02 17:41:21 -0400

    working script title

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,16 +5,25 @@ const nav = document.querySelector('.nav-container');
 const aboutLink = document.querySelector('.aboutLink');
 const aboutDiv = document.querySelector('.aboutDiv');
 const heroDiv = document.querySelector('.hero-container');
+const eventDiv = document.querySelector('.all-events');
 
 window.addEventListener('scroll', function() {
     let coords = elem.getBoundingClientRect();
     let coordsAbout = aboutDiv.getBoundingClientRect();
     let coordsTitle = title.getBoundingClientRect(); 
+    let coordsEvent = eventDiv.getBoundingClientRect();
+    const bellowEvent = coordsTitle.bottom + pageYOffset > coordsEvent.top + pageYOffset;
+    const aboveEvent = coordsTitle.bottom + pageYOffset > coordsEvent.bottom + pageYOffset;
     const belowTop = coordsTitle.bottom + pageYOffset > coords.top + pageYOffset;
-    const aboveBottom = coordsTitle.bottom + pageYOffset > coords.bottom + pageYOffset;
+    const aboveBottom = coordsTitle.bottom + pageYOffset > coords.bottom + pageYOffset; //member
     const aboveTop = coordsTitle.bottom + pageYOffset > coordsAbout.top + pageYOffset;
     const belowAbove = coordsTitle.bottom + pageYOffset > coordsAbout.bottom + pageYOffset;
-    if (belowTop && !aboveBottom){
+    if(bellowEvent && !belowTop) {
+      title.innerHTML = 'NEWS';
+      title.style.display = "inline-block";
+      nav.style.display = "inline-block";
+    }
+    else if (belowTop && bellowEvent && !aboveBottom){
      title.innerHTML = 'MEMBERS';
      title.style.display = "inline-block";
      nav.style.display = "inline-block";
