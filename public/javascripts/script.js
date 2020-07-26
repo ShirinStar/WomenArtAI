9fbf50cef5aa59e8268174e2c4d2ce149b0f11a5
shirin <shirinanlen@gmail.com>
2020-07-26 13:47:35 -0400

    removing event section

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,57 +15,53 @@ window.addEventListener('scroll', function() {
     let coords = elem.getBoundingClientRect();
     let coordsAbout = aboutDiv.getBoundingClientRect();
     let coordsTitle = title.getBoundingClientRect(); 
-    let coordsEvent = eventDiv.getBoundingClientRect();
-    const bellowEvent = coordsTitle.bottom + pageYOffset > coordsEvent.top + pageYOffset;
-    const aboveEvent = coordsTitle.bottom + pageYOffset > coordsEvent.bottom + pageYOffset;
+    // let coordsEvent = eventDiv.getBoundingClientRect(); // event section
+    // const bellowEvent = coordsTitle.bottom + pageYOffset > coordsEvent.top + pageYOffset; // event section
+    // const aboveEvent = coordsTitle.bottom + pageYOffset > coordsEvent.bottom + pageYOffset;// event section
     const belowTop = coordsTitle.bottom + pageYOffset > coords.top + pageYOffset;
     const aboveBottom = coordsTitle.bottom + pageYOffset > coords.bottom + pageYOffset; //member
     const aboveTop = coordsTitle.bottom + pageYOffset > coordsAbout.top + pageYOffset;
     const belowAbove = coordsTitle.bottom + pageYOffset > coordsAbout.bottom + pageYOffset;
-    if(bellowEvent && !belowTop) {
-      title.innerHTML = 'NEWS';
-      title.style.display = "inline-block";
-      nav.style.display = "inline-block";
-    }
-    else if (belowTop && bellowEvent && !aboveBottom){
+    // if(bellowEvent && !belowTop) { related to event section 
+    //   title.innerHTML = 'NEWS';
+    //   title.style.display = "inline-block";
+    //   nav.style.display = "inline-block";
+    // }
+    // else if (belowTop && bellowEvent && !aboveBottom){ // when bringing back event
+
+     if (belowTop && !aboveBottom){
      title.innerHTML = 'MEMBERS';
      title.style.display = "inline-block";
-     nav.style.display = "inline-block";
    } else if(aboveTop && !belowAbove) {
       title.innerHTML = 'ABOUT';
-      nav.style.display = "inline-block";
       title.style.display = "inline-block";
    } else {
      title.innerHTML = 'WOMXN ART AI';
-     nav.style.display = "inline-block";
      title.style.display = "inline-block";
    }
  });
 
-let showMoreText = true;
- function showHiddenEvents() {
-  const hiddenEvents = document.querySelectorAll('.event-list.hidden-event');
-  hiddenEvents.forEach(element => {
-    if (showMoreText === true) {
-      element.style.display = "block";
-    } else {
-      element.style.display = "none";
-    }
-  })
-  if (!showMoreText) {
-    showMore.innerHTML = "Show more events...";
-  } else {
-    showMore.innerHTML = "Show less events";
-  }
-  showMoreText = !showMoreText;
-}
- showMore.addEventListener('click', showHiddenEvents)
-
+// let showMoreText = true;
+//  function showHiddenEvents() {
+//   const hiddenEvents = document.querySelectorAll('.event-list.hidden-event');
+//   hiddenEvents.forEach(element => {
+//     if (showMoreText === true) {
+//       element.style.display = "block";
+//     } else {
+//       element.style.display = "none";
+//     }
+//   })
+//   if (!showMoreText) {
+//     showMore.innerHTML = "Show more events...";
+//   } else {
+//     showMore.innerHTML = "Show less events";
+//   }
+//   showMoreText = !showMoreText;
+// }
+//  showMore.addEventListener('click', showHiddenEvents)
 
 function myFunction() {
-  console.log('click')
   const menu = document.querySelector('.menu');
-
   if (menu.classList.contains('active')) {
     menu.classList.remove('active');
   } else {
