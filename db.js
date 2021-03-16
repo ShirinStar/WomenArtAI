75672419c59baddc85427211e5b7a51b8ac917e6
shirinstar <shirinanlen@gmail.com>
2021-03-15 21:46:51 -0700

    last try

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const pgp = require ('pg-promise');
-pgp.pg.defaults.ssl = true;
 
 
-const db = pgp()((process.env.DATABASE_URL + "?ssl=true") || {
+const db = pgp({
+  ssl: {
+    rejectUnauthorized: false
+  }
+})((process.env.DATABASE_URL) || {
   host:'localhost', database: 'womenai'
 })
 
